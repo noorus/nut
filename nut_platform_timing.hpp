10d510fcd08eadf727bbb58d0e17550c6fbc6c7e
noorus <noorus@users.noreply.github.com>
2024-06-20 04:16:23 +0300

    Add nut_platform_timing.hpp

---
+++ nut_platform_timing.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "nut_types.hpp"
+
+namespace nut {
+
+  namespace platform {
+
+    //! \class PerformanceTimer
+    //! Native performance timer implementation for high-precision timing of tasks.
+    class PerformanceTimer {
+    private:
+      LARGE_INTEGER frequency_;
+      LARGE_INTEGER time_;
+    public:
+      PerformanceTimer(): frequency_ { { 0, 0 } }, time_ { { 0, 0 } }
+      {
+        if ( !QueryPerformanceFrequency( &frequency_ ) )
+          NUT_RUNTIME_EXCEPT( "Couldn't query HPC frequency" );
+      }
+      void start()
+      {
+        QueryPerformanceCounter( &time_ );
+      }
+      double stop()
+      {
+        LARGE_INTEGER newTime;
+        QueryPerformanceCounter( &newTime );
+        auto delta = ( newTime.QuadPart - time_.QuadPart ) * 1000000;
+        delta /= frequency_.QuadPart;
+        double ms = (double)delta / 1000.0;
+        return ms;
+      }
+    };
+
+    //! \class PerformanceClock
+    //! Native performance clock implementation for high-precision loop delta timing.
+    class PerformanceClock {
+    private:
+      LARGE_INTEGER current_;
+      double frequency_ = 0.0;
+      double inverse_ = 0.0;
+    public:
+      PerformanceClock(): current_ { { 0, 0 } }
+      {
+        LARGE_INTEGER frequency;
+        if ( !QueryPerformanceFrequency( &frequency ) )
+          NUT_RUNTIME_EXCEPT( "Couldn't query HPC frequency" );
+        frequency_ = static_cast<double>( frequency.QuadPart );
+        inverse_ = ( 1000000.0 / frequency.QuadPart );
+      }
+      //! Call this once before entering whatever loop is going to then keep calling update()
+      inline void init()
+      {
+        QueryPerformanceCounter( &current_ );
+      }
+      //! Returns the time in seconds since the last call to update()
+      inline double update()
+      {
+        LARGE_INTEGER new_;
+        QueryPerformanceCounter( &new_ );
+        auto delta = ( new_.QuadPart - current_.QuadPart );
+        current_ = new_;
+        return ( static_cast<double>( delta ) / frequency_ );
+      }
+      //! Returns the time in seconds since the last call to update() without "resetting" the counter
+      inline double peek() const
+      {
+        LARGE_INTEGER new_;
+        QueryPerformanceCounter( &new_ );
+        auto delta = ( new_.QuadPart - current_.QuadPart );
+        return ( static_cast<double>( delta ) / frequency_ );
+      }
+      //! Returns the time in microseconds since the last call to update() without "resetting" the counter
+      inline uint64_t peekMicroseconds() const
+      {
+        LARGE_INTEGER new_;
+        QueryPerformanceCounter( &new_ );
+        auto delta = ( new_.QuadPart - current_.QuadPart );
+        return static_cast<uint64_t>( delta * inverse_ );
+      }
+      //! Returns the time in milliseconds since the last call to update() without "resetting" the counter
+      inline double peekMilliseconds() const
+      {
+        return static_cast<double>( peekMicroseconds() ) * 0.001;
+      }
+    };
+
+  }
+
+}
